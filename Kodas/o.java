ac837535c6c0faa0680780ff91b43a806c8cbef5
Paulius Šukys <paul.sukys@gmail.com>
2015-05-03 12:34:37 +0300

    Pakankamai kreivas įrankis praktiškai nieko nesugeneruoja

---
+++ Kodas/o.java
@@ -0,0 +1,26 @@
+class SAVSInformacija {
+}
+class Zurnalas {
+}
+class Kamera {
+}
+class MokejimoAparatas {
+}
+class Slagbaumas {
+}
+class Valdiklis {
+}
+class TinklasISAVS {
+}
+class AdminPanele extends Identifikacija {
+}
+class BendraPanele extends NetSasaja {
+}
+class TSPTPanele extends Identifikacija {
+}
+class NetSasaja extends TinklasISAVS {
+}
+class Identifikacija {
+}
+class SAVSMain {
+}
